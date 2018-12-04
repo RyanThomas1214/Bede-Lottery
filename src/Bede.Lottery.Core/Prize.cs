64db9e6bd97c976a2beaf77a4845c3ec1b474c5d
RyanThomas <ryan.thomas@bedegaming.com>
2018-12-04 13:38:29 +0000

    Add Business Objects

---
+++ src/Bede.Lottery.Core/Prize.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bede.Lottery.Core
+{
+	class Prize
+	{
+		public string Name { get; set; }
+		public int Amount { get; set; }
+	}
+}
---
+++ src/Bede.Lottery.Core/Winner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bede.Lottery.Core
+{
+	class Winner
+	{
+		public Employee WinningEmployee { get; set; }
+		public Prize WinningPrize { get; set; }
+	}
+}
